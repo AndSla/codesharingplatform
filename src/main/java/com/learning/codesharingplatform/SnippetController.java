8040f1fd7fa9f133672132eecb63159cc0fd2a67
Andrzej <annddrzej@gmail.com>
2022-05-16 19:07:50 +0200

    Implement memory db for storing snippets, implement getting snippets by id

--- src/main/java/com/learning/codesharingplatform/SnippetController.java
+++ src/main/java/com/learning/codesharingplatform/SnippetController.java
@@ -3,40 +3,46 @@ package com.learning.codesharingplatform;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class SnippetController {
     private final Snippet snippet = new Snippet();
+    private final Map<Integer, Snippet> snippetsDB = new HashMap<>();
 
     {
         snippet.setCode("public static void main(String[] args) {\n" +
                 "    SpringApplication.run(CodeSharingPlatform.class, args);\n" +
                 "}");
+        snippetsDB.put(snippetsDB.size() + 1, snippet);
     }
 
-    @GetMapping("/api/code")
+    @GetMapping("/api/code/{id}")
     @ResponseBody
-    public Snippet getCodeJson() {
-        return snippet;
+    public Snippet getCodeJson(@PathVariable int id) {
+        return snippetsDB.get(id);
     }
 
-    @GetMapping(value = "/code", produces = "text/html")
-    public String getCodeHtml(Model model) {
-        model.addAttribute("date", snippet.getDate());
-        model.addAttribute("code", snippet.getCode());
+    @GetMapping(value = "/code/{id}", produces = "text/html")
+    public String getCodeHtml(@PathVariable int id, Model model) {
+        model.addAttribute("date", snippetsDB.get(id).getDate());
+        model.addAttribute("code", snippetsDB.get(id).getCode());
         return "getSnippet";
     }
 
     @PostMapping("/api/code/new")
-    public ResponseEntity<?> postSnippet(@RequestBody Snippet snippet) {
-        this.snippet.setCode(snippet.getCode());
-        return ResponseEntity
-                .ok()
-                .body(new Empty());
+    @ResponseBody
+    public Map<String,Integer> postSnippet(@RequestBody Snippet snippet) {
+        Snippet newSnippet = new Snippet();
+        newSnippet.setCode(snippet.getCode());
+        int id = snippetsDB.size() + 1;
+        snippetsDB.put(id, newSnippet);
+        Map<String, Integer> response = new HashMap<>();
+        response.put("id", id);
+        return response;
     }
 
     @GetMapping(value = "/code/new", produces = "text/html")
